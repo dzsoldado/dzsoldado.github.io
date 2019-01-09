b2097e0b5006416a6d3b4c4c3494b669b1879e16
O. Ben <klemidz@gmail.com>
2019-01-10 00:48:03 +0100

    Update main.js

--- quotes/js/main.js
+++ quotes/js/main.js
@@ -1,20 +1,28 @@
 $(document).ready(function(){
-  twlink=""; 
-   
-     $("#getQuote").on("click",function(){
-       $.getJSON("https://talaikis.com/api/quotes/random/ ",function(json){
-         $(".quote").animate({ opacity: 0}, 300,function() {
-           $(".quote").animate({ opacity: 1}, 300);
-           $("#quote-text").html(json.quote);
-           $("#quote-author").html(json.author);
-         });
-         
-         //Twitter share
-         twlink ='https://twitter.com/intent/tweet?text=' +'" '+json.quote+' " '+" - "+ json.author +"&hashtags=Quote";
-         $("a.twitter").attr("href",twlink);
-         $("a.twitter").attr("onclick","return !window.open(this.href, 'Twitter', 'width=640,height=300') ");
- 
-         })
-      });
-      jQuery('#getQuote').click();     
+ twlink=""; 
+  
+    $("#getQuote").on("click",function(){
+           
+       $.ajax({
+            url: "https://api.forismatic.com/api/1.0/?",
+            dataType: "jsonp",
+            data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
+            success: function( response ) {
+              $(".quote").animate({ opacity: 0}, 300,function() {
+                $(".quote").animate({ opacity: 1}, 300);
+                $("#quote-text").html(response.quoteText);
+                $("#quote-author").html(response.quoteAuthor);
+              });
+              
+              twlink ='https://twitter.com/intent/tweet?text=' +'" '+response.quoteText+' " '+" - "+ response.quoteAuthor +"&hashtags=Quote";
+              $("a.twitter").attr("href",twlink);
+              $("a.twitter").attr("onclick","return !window.open(this.href, 'Twitter', 'width=640,height=300') ");
+            }
+        });
+    });
+  
+        jQuery('#getQuote').click();     
+
  });
+    
+        
